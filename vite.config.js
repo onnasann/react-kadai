8f78d7eb68d5af62aa01d477307ab49336b6fa9f
onnnasan <chsh24020@gmail.com>
2026-01-08 16:30:07 +0900

    Include dist for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "/react-kadai/",   // ← これを追加（リポジトリ名）
   plugins: [react()],
 });
